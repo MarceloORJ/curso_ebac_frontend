8d7f75b2d8aab47aa95beaafa5abc778c726a1c5
Marcelo_orj <128622215+MarceloORJ@users.noreply.github.com>
2023-12-04 17:42:38 -0300

    Add files via upload

---
+++ exercicio.ts
@@ -0,0 +1,13 @@
+namespace exercicio{
+    function calcularMultiplicacao(primeiroValor: number, segundoValor: number): number{
+        primeiroValor = 10;
+        segundoValor = 2;
+        return primeiroValor * segundoValor;
+    }
+    
+    function saudacao(nome:string): string{
+        nome = "Marcelo";
+        return "Olá " + nome;
+    }
+
+}
